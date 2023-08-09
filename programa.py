793da353b5ca86edeb4ec24bf65a21f181e6bb38
Sad <dedecsilva2017@gmail.com>
2023-08-09 00:54:36 -0300

    09.08.2023
    - POUCAS MELHORIAS
    - DESCRIÇÃO MELHOR DAS THREADS
    - ORGANIZAÇÃO MELHOR
    - MELHORIA DO AUTO ATTACH FLUXUS

--- programa.py
+++ programa.py
@@ -7,8 +7,6 @@ import colorama
 import datetime
 import cv2
 import ctypes
-import sys
-from ctypes import wintypes as wts
 
 colorama.init(convert=True)
 
@@ -38,8 +36,9 @@ limite_de_processos_abertos = int(input("DEFINA QUANTAS INSTÂNCIAS ABERTAS PARA
 print()
 delay = int(10800)
 
-def Arrumar_janelas(processo_roblox, limite_de_processos_abertos):
+def Arrumar_janelas(limite_de_processos_abertos):
     while True:
+        
         def count_processes_by_name(processo_roblox):
             count = 0
             for process in psutil.process_iter(['name']):
@@ -48,69 +47,20 @@ def Arrumar_janelas(processo_roblox, limite_de_processos_abertos):
             return count
 
         current_count = count_processes_by_name(processo_roblox)
-
+        
         if current_count >= limite_de_processos_abertos:    
             time.sleep(5)
             user32 = ctypes.WinDLL("user32.dll")
 
-            HWND = wts.HWND
-            UINT = wts.UINT
-
             hwnd_desktop = user32.GetDesktopWindow()
             user32.TileWindows(hwnd_desktop, 0x0001, None, 0, None)
             user32.TileWindows(hwnd_desktop, 0x0002, None, 0, None)
-                    
+                        
             time.sleep(300)
 
-def Injetar_fluxus(processo_roblox, limite_de_processos):
+def Injetar_fluxus(limite_de_processos):
      while True:
-        fluxusWindow = pygetwindow.getWindowsWithTitle("MainWindow")
-        if fluxusWindow:
-            fluxusWindow = fluxusWindow[0]
-            fluxusWindow.restore()
-            time.sleep(5)
-                
-            ok_erro_button = pyautogui.locateOnScreen(ok_erro, confidence=0.7)
-
-            if ok_erro_button:
-                pyautogui.click(ok_erro_button)
-                time.sleep(2)
-                ok_erro_button = pyautogui.locateOnScreen(ok_erro, confidence=0.7)
-                pyautogui.click(ok_erro_button)
-                time.sleep(2)
-                inject_button = pyautogui.locateOnScreen(inject_button_img, confidence=0.7)
-                pyautogui.click(inject_button)
-                time.sleep(5)
-                fluxusWindow.minimize()
-                print_with_timestamp ("FLUXUS INJETADO E ERROS FECHADOS")
-                print()
-            else:
-                inject_button = pyautogui.locateOnScreen(inject_button_img, confidence=0.7)
-                pyautogui.click(inject_button)
-                time.sleep(5)
-                fluxusWindow.minimize()
-                print_with_timestamp ("FLUXUS INJETADO")
-                print()
-        else:
-            print_with_timestamp ("FLUXUS NÃO ENCONTRADO")
-            print()
-
-        time.sleep(30)
-
-def Fechar_erros_do_account_manager(processo_cmd):
-    while True:
-        for process in psutil.process_iter(['name']):
-            if process.info['name'] == processo_cmd:
-                process.kill()
-                print_with_timestamp ("ERROS DO ACCOUNT MANAGER FECHADOS")
-                print()
-            else:
-                print_with_timestamp ("SEM ERROS DO ACCOUNT MANAGER")
-                print()
-            time.sleep(300)
-
-def Printar_quantas_janelas_estão_abertas(processo_roblox):
-    while True:
+         
         def count_processes_by_name(processo_roblox):
             count = 0
             for process in psutil.process_iter(['name']):
@@ -119,7 +69,54 @@ def Printar_quantas_janelas_estão_abertas(processo_roblox):
             return count
 
         current_count = count_processes_by_name(processo_roblox)
+        
+        if current_count >= limite_de_processos:
+            time.sleep(5)
+            fluxusWindow = pygetwindow.getWindowsWithTitle("MainWindow")
+            if fluxusWindow:
+                fluxusWindow = fluxusWindow[0]
+                fluxusWindow.restore()
+                time.sleep(5)
+                    
+                ok_erro_button = pyautogui.locateOnScreen(ok_erro, confidence=0.7)
 
+                if ok_erro_button:
+                    pyautogui.click(ok_erro_button)
+                    time.sleep(2)
+                    ok_erro_button = pyautogui.locateOnScreen(ok_erro, confidence=0.7)
+                    pyautogui.click(ok_erro_button)
+                    time.sleep(2)
+                    inject_button = pyautogui.locateOnScreen(inject_button_img, confidence=0.7)
+                    pyautogui.click(inject_button)
+                    time.sleep(5)
+                    fluxusWindow.minimize()
+                    print_with_timestamp ("FLUXUS INJETADO E ERROS FECHADOS")
+                    print()
+                else:
+                    inject_button = pyautogui.locateOnScreen(inject_button_img, confidence=0.7)
+                    pyautogui.click(inject_button)
+                    time.sleep(5)
+                    fluxusWindow.minimize()
+                    print_with_timestamp ("FLUXUS INJETADO")
+                    print()
+            else:
+                print_with_timestamp ("FLUXUS NÃO ENCONTRADO")
+                print()
+
+        time.sleep(30)
+
+def Printar_quantas_janelas_estão_abertas():
+    while True:
+        
+        def count_processes_by_name(processo_roblox):
+            count = 0
+            for process in psutil.process_iter(['name']):
+                if process.info['name'] == processo_roblox:
+                    count += 1
+            return count
+
+        current_count = count_processes_by_name(processo_roblox)
+        
         if current_count == 0:
             print_with_timestamp ("NENHUMA INSTÂNCIA ESTÁ ABERTA")
             print ()
@@ -133,8 +130,20 @@ def Printar_quantas_janelas_estão_abertas(processo_roblox):
                 print_with_timestamp (f"{current_count} INSTÂNCIAS ESTÃO ABERTAS")
                 print ()
                 time.sleep(10)
+                
+def Fechar_erros_do_account_manager(processo_cmd):
+    while True:
+        time.sleep(300)
+        for process in psutil.process_iter(['name']):
+            if process.info['name'] == processo_cmd:
+                process.kill()
+                print_with_timestamp ("ERROS DO ACCOUNT MANAGER FECHADOS")
+                print()
+            else:
+                print_with_timestamp ("SEM ERROS DO ACCOUNT MANAGER")
+                print()
 
-def Fechar_todas_instâncias_a_cada_determinado_tempo(delay):
+def Fechar_todas_instâncias_a_cada_determinado_tempo(processo_roblox, delay):
     while True:
         time.sleep(delay)
         for process in psutil.process_iter(['name']):
@@ -143,21 +152,28 @@ def Fechar_todas_instâncias_a_cada_determinado_tempo(delay):
                 print_with_timestamp ("TODAS INSTÂNCIAS FORAM FECHADAS")
                 print()
         
+# Lógica de execução das threads
 
-thread1 = threading.Thread(target=Arrumar_janelas, args=(processo_roblox, limite_de_processos_abertos))
-thread2 = threading.Thread(target=Injetar_fluxus, args=(processo_roblox, limite_de_processos_abertos))
-thread3 = threading.Thread(target=Fechar_todas_instâncias_a_cada_determinado_tempo, args=(delay,))
-thread4 = threading.Thread(target=Printar_quantas_janelas_estão_abertas, args=(processo_roblox,))
-thread5 = threading.Thread(target=Fechar_erros_do_account_manager,args=(processo_cmd,))
+# 1° - Injetar fluxus para printar em primeiro lugar se o auto attach está ativado ou desativado
+# 2° - Printar as janelas abertas, pois é uma informação fundamental pro programa
+# 3° - Arrumar janelas, porque depois de injetar o fluxus é a função principal do programa
+# 4° - Fechar erros do account manager pois, depois de arrumar as janelas é a outra principal função do programa
+# 5° - Por final fechar todas as instâncias, pois é a coisa que mais demora pro programa fazer, como se ela fosse a última camada que ele executa
+
+Thread_arrumar_janelas = threading.Thread(target=Arrumar_janelas, args=(limite_de_processos_abertos,))
+Thread_injetar_fluxus = threading.Thread(target=Injetar_fluxus, args=(limite_de_processos_abertos,))
+Thread_fechar_todas_instâncias_a_cada_determinado_tempo = threading.Thread(target=Fechar_todas_instâncias_a_cada_determinado_tempo, args=(processo_roblox, delay))
+Thread_printar_quantas_janelas_estão_abertas = threading.Thread(target=Printar_quantas_janelas_estão_abertas)
+Thread_fechar_erros_do_account_manager = threading.Thread(target=Fechar_erros_do_account_manager,args=(processo_cmd,))
 
 if injetar_fluxus_ou_não == 1:
-    thread2.start()
+    Thread_injetar_fluxus.start()
     print(colorama.Back.GREEN + " AUTO ATTACH FLUXUS ATIVADO " + colorama.Back.RESET)
     print()
 else:
     print(colorama.Back.RED + " AUTO ATTACH FLUXUS DESATIVADO " + colorama.Back.RESET)
     print()
-thread1.start()
-thread3.start()
-thread4.start()
-thread5.start()
+Thread_printar_quantas_janelas_estão_abertas.start()
+Thread_arrumar_janelas.start()
+Thread_fechar_erros_do_account_manager.start()
+Thread_fechar_todas_instâncias_a_cada_determinado_tempo.start()
--- tests.py
+++ tests.py
@@ -1,29 +1,7 @@
-import pyautogui
-import pygetwindow
-import time
+import ctypes
 
-ok_erro = "Error button image.png"
-inject_button_img = "Inject button image.png"
+user32 = ctypes.WinDLL("user32.dll")
 
-fluxusWindow = pygetwindow.getWindowsWithTitle("MainWindow")[0]
-fluxusWindow.restore()
-time.sleep(5)
-ok_erro_button = pyautogui.locateOnScreen(ok_erro, confidence=0.7)
-
-if ok_erro_button:
-    pyautogui.click(ok_erro_button)
-    time.sleep(2)
-    ok_erro_button = pyautogui.locateOnScreen(ok_erro, confidence=0.7)
-    pyautogui.click(ok_erro_button)
-    time.sleep(2)
-    inject_button = pyautogui.locateOnScreen(inject_button_img, confidence=0.7)
-    pyautogui.click(inject_button)
-    time.sleep(5)
-    fluxusWindow.minimize()
-    print("FLUXUS INJETADO E ERRO FECHADO")
-else:
-    inject_button = pyautogui.locateOnScreen(inject_button_img, confidence=0.7)
-    pyautogui.click(inject_button)
-    time.sleep(5)
-    fluxusWindow.minimize()
-    print("FLUXUS INJETADO")
+hwnd_desktop = user32.GetDesktopWindow()
+user32.TileWindows(hwnd_desktop, 0x0001, None, 0, None)
+user32.TileWindows(hwnd_desktop, 0x0002, None, 0, None)
